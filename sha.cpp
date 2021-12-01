e07fe29e5677d86c42b9cdc3a6cd9fe10a54d132
David Freifeld <freifeld.david@gmail.com>
2021-12-01 00:07:05 -0800

    Initial naive SHA-256
    Isn't even deterministic.

--- sha.cpp
+++ sha.cpp
@@ -2,10 +2,15 @@
 #include <cstddef>
 #include <cstring>
 #include <array>
+#include <bit>
+#include <iostream>
+#include <iomanip>
+#include <fmt/format.h> 
 
 using Hash = std::array<uint8_t, 256>;
 
-Hash sha_256(uint8_t* bytes, size_t len) {
+// Blatantly adapted from https://qvault.io/cryptography/how-sha-2-works-step-by-step-sha-256/
+void sha_256(char* bytes, size_t len) {
     size_t content_len = len + 64 + 1;
     size_t buffer_len = 0;
     while (true) {
@@ -13,9 +18,9 @@ Hash sha_256(uint8_t* bytes, size_t len) {
 	if (buffer_len > content_len) break;
     }
     uint8_t buffer[buffer_len];
-    memcpy(&buffer, bytes, len);
+    memcpy(buffer, bytes, len);
     buffer[len+1] = 0b1000'0000;
-    memcpy(&buffer+buffer_len-64, &len, sizeof(size_t));
+    memcpy(buffer+buffer_len-64, &len, sizeof(size_t));
 
     uint32_t h0 = 0x6a09e667;
     uint32_t h1 = 0xbb67ae85;
@@ -37,6 +42,61 @@ Hash sha_256(uint8_t* bytes, size_t len) {
 	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
     };
 
-    uint32_t w[len/4 + 48];
-    memcpy(&buffer, &w, buffer_len);
+    for (int i = 0; i < buffer_len; i += 512) {    
+	size_t wlen = 512/4 + 48;
+	uint32_t w[wlen];
+	memcpy(buffer+i, &w, 512);
+	
+	for (int i = buffer_len; i < wlen; i++) {
+	    uint32_t s0 = (std::rotr(w[i-15], 7) ^ std::rotr(w[i-15], 18) ^ std::rotr(w[i-15], 3));
+	    uint32_t s1 = (std::rotr(w[i-2], 17) ^ std::rotr(w[i-2], 19)  ^ std::rotr(w[i-2], 10));
+	    w[i] = w[i-16] + s0 + w[i-7] + s1;
+	}
+
+	uint32_t a = h0, b = h1, c = h2, d = h3, e = h4, f = h5, g = h6, h = h7;
+	for (int i = 0; i < 64; i++) {
+	    uint32_t s1 = (std::rotr(e, 6) ^ std::rotr(e, 11) ^ std::rotr(e, 25));
+	    uint32_t ch = (e & f) ^ ((~e) & g);
+	    uint32_t temp1 = h + s1 + ch + k[i] + w[i];
+	    uint32_t s0 = (std::rotr(a, 2) ^ std::rotr(a, 13) ^ std::rotr(a, 22));
+	    uint32_t maj = (a & b) ^ (a & c) ^ (b & c);
+	    uint32_t temp2 = s0 + maj;
+	    h = g;
+	    g = f;
+	    f = e;
+	    e = d + temp1;
+	    d = c;
+	    c = b;
+	    b = a;
+	    a = temp1 + temp2;
+	}
+
+	h0 += a;
+	h1 += b;
+	h2 += c;
+	h3 += d;
+	h4 += e;
+	h5 += f;
+	h6 += g;
+	h7 += h;
+
+	char out[32];
+	memcpy(out, &h0, sizeof(uint32_t));
+	memcpy(out+sizeof(uint32_t)*1, &h1, sizeof(uint32_t));
+	memcpy(out+sizeof(uint32_t)*2, &h2, sizeof(uint32_t));
+	memcpy(out+sizeof(uint32_t)*3, &h3, sizeof(uint32_t));
+	memcpy(out+sizeof(uint32_t)*4, &h4, sizeof(uint32_t));
+	memcpy(out+sizeof(uint32_t)*5, &h5, sizeof(uint32_t));
+	memcpy(out+sizeof(uint32_t)*6, &h6, sizeof(uint32_t));
+	memcpy(out+sizeof(uint32_t)*7, &h7, sizeof(uint32_t));
+
+	for (int i = 0; i < 32; i++) {
+	    std::cout << fmt::format("{:02x}", (uint8_t)out[i]);
+	}
+	std::cout << '\n';
+    }    
+}
+
+int main() {
+    sha_256(const_cast<char*>("hello world"), 11);
 }
