98ee76930b2e1361fa402e08b73503b7367021f9
David Freifeld <freifeld.david@gmail.com>
2021-12-08 23:00:45 -0800

    Minor bug fixing but still same major errors

--- sha.cpp
+++ sha.cpp
@@ -28,7 +28,7 @@ void dump_array(T* bytes, size_t len) {
 
 // Blatantly adapted from https://qvault.io/cryptography/how-sha-2-works-step-by-step-sha-256/
 void sha_256(char* bytes, uint32_t len) {
-    size_t content_len = len + 1 + 1;
+    size_t content_len = len + 1 + 4;
     size_t buffer_len = 0;
     while (true) {
 	buffer_len += 64;
@@ -36,7 +36,8 @@ void sha_256(char* bytes, uint32_t len) {
     }
     uint8_t* buffer = (uint8_t*)calloc(buffer_len, sizeof(uint8_t));
     memcpy(buffer, bytes, len);
-    buffer[len] = 0b1000'0000;
+    std::cout << len << " " << buffer_len << "\n";
+    buffer[len] = 0b10000000;
     
     *((uint32_t*)buffer + buffer_len/4 - 1) = __builtin_bswap32(len);
 
@@ -44,14 +45,16 @@ void sha_256(char* bytes, uint32_t len) {
     for (int i = 0; i < 4; i++) {
 	buffer[buffer_len-i] = std::rotr(buffer[buffer_len-i], 5);
     }
-    //dump_array<uint8_t>(buffer, buffer_len);
+
+    // auto c_buffer = const_cast<const uint8_t*>(buffer);
+    dump_array<uint8_t>(buffer, buffer_len);
     //std::cout << buffer_len << '\n';
     uint32_t h[8] = {
 	0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
 	0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
     };
 
-    uint64_t k[64] = {
+    const uint64_t k[64] = {
 	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
 	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -68,22 +71,26 @@ void sha_256(char* bytes, uint32_t len) {
 	uint32_t* w = (uint32_t*)calloc(wlen, sizeof(uint32_t));
 	memcpy(w, buffer+i, 64);
 	for (int i = 0; i < wlen; i++) {
-	    w[i] = __builtin_bswap32(w[i]);
+	    w[i] = __builtin_bswap32(w[i]); 
 	}
-	
+
+	// Likely not the problem - doesn't vary across chunks 
 	for (int i = 16; i < wlen; i++) {
 	    uint32_t s0 = (std::rotr(w[i-15], 7) ^ std::rotr(w[i-15], 18) ^ (w[i-15] >> 3));
 	    uint32_t s1 = (std::rotr(w[i-2], 17) ^ std::rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
 	    w[i] = w[i-16] + s0 + w[i-7] + s1;
 	}
-	// dump_array<uint32_t>(w, wlen);
+	dump_array<uint32_t>(w, wlen);
+
+	// Cast to constant so nothing can frick with it - just for paranoia's sake.
+	auto c_w = const_cast<const uint32_t*>(w);
 
 	uint32_t a[8];
 	memcpy(a, h, sizeof(uint32_t)*8);
 	for (int i = 0; i < 64; i++) {
 	    uint32_t s1 = (std::rotr(a[4], 6) ^ std::rotr(a[4], 11) ^ std::rotr(a[4], 25));
 	    uint32_t ch = (a[4] & a[5]) ^ ((~a[4]) & a[6]);
-	    uint32_t temp1 = a[7] + s1 + ch + k[i] + w[i];
+	    uint32_t temp1 = a[7] + s1 + ch + k[i] + c_w[i];
 	    uint32_t s0 = (std::rotr(a[0], 2) ^ std::rotr(a[0], 13) ^ std::rotr(a[0], 22));
 	    uint32_t maj = (a[0] & a[1]) ^ (a[0] & a[2]) ^ (a[1] & a[2]);
 	    uint32_t temp2 = s0 + maj;
@@ -96,9 +103,10 @@ void sha_256(char* bytes, uint32_t len) {
 	    a[1] = a[0];
 	    a[0] = temp1 + temp2;
 	}
-	for (int i = 0; i < 8; i++) h[i] += a[i];
+	for (int i = 0; i < 8; i++) h[i] = h[i] + a[i];
 	free(w);
     }
+
     char out[32] = {0};
     memcpy(out, h, sizeof(uint32_t)*8);
     
