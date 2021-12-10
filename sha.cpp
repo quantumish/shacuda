db6d2ceafa936bfacb40ac18d75c1c0972df0918
David Freifeld <freifeld.david@gmail.com>
2021-12-09 23:43:00 -0800

    Beginning cuda-fication

--- sha.cpp
+++ sha.cpp
@@ -16,6 +16,10 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <fmt/format.h>
+#include <cuda_runtime_api.h>
+#include <cuda.h>
+
+#define rotr(a,b) (((a) >> (b)) | ((a) << (32-(b))))
 
 template<typename T>
 void dump_array(T* bytes, size_t len) {
@@ -27,25 +31,11 @@ void dump_array(T* bytes, size_t len) {
     std::cout << '\n';
 }
 
-constexpr uint64_t k[64] = {
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-};
-
 using Hash = std::unique_ptr<uint8_t[]>;
 
 struct sha_ctx {
     // std::unique_ptr<uint8_t[]> buffer;
-    uint32_t hash[8] = {
-	0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-	0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
-    };
+    uint32_t* hash;
     uint64_t len;
     
     sha_ctx(uint64_t len);
@@ -54,19 +44,41 @@ struct sha_ctx {
     void dump_hash();
 };
 
-sha_ctx::sha_ctx(uint64_t length) :hash{0}, len(length) {}
+sha_ctx::sha_ctx(uint64_t length) :len(length) {
+    cudaMallocManaged((void**)&hash, 8*sizeof(uint32_t));    
+    hash[0] = 0x6a09e667;
+    hash[1] = 0xbb67ae85;
+    hash[2] = 0x3c6ef372;
+    hash[3] = 0xa54ff53a;
+    hash[4] = 0x510e527f;
+    hash[5] = 0x9b05688c;
+    hash[6] = 0x1f83d9ab;
+    hash[7] = 0x5be0cd19;
+}
 
 #define NUM_CHUNKS 8192
 
-void sha_ctx::process(uint8_t* bytes) {
+__global__
+void process(uint8_t* bytes, uint32_t* hash) {
     for (int i = 0; i < NUM_CHUNKS; i += 1) {
+	uint64_t k[64] = {
+	    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+	};
+	
 	uint32_t w[64] = {0};
 	memcpy(w, bytes+(i*64), 64);
 	for (int i = 0; i < 16; i++) w[i] = __builtin_bswap32(w[i]);
 
 	for (int i = 16; i < 64; i++) {
-	    uint32_t s0 = (std::rotr(w[i-15], 7) ^ std::rotr(w[i-15], 18) ^ (w[i-15] >> 3));
-	    uint32_t s1 = (std::rotr(w[i-2], 17) ^ std::rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
+	    uint32_t s0 = (rotr(w[i-15], 7) ^ rotr(w[i-15], 18) ^ (w[i-15] >> 3));
+	    uint32_t s1 = (rotr(w[i-2], 17) ^ rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
 	    w[i] = w[i-16] + s0 + w[i-7] + s1;
 	}
 
@@ -74,10 +86,10 @@ void sha_ctx::process(uint8_t* bytes) {
 	memcpy(a, hash, sizeof(uint32_t)*8);
 
 	for (int i = 0; i < 64; i++) {
-	    uint32_t s1 = (std::rotr(a[4], 6) ^ std::rotr(a[4], 11) ^ std::rotr(a[4], 25));
+	    uint32_t s1 = (rotr(a[4], 6) ^ rotr(a[4], 11) ^ rotr(a[4], 25));
 	    uint32_t ch = (a[4] & a[5]) ^ ((~a[4]) & a[6]);
 	    uint32_t temp1 = a[7] + s1 + ch + k[i] + w[i];
-	    uint32_t s0 = (std::rotr(a[0], 2) ^ std::rotr(a[0], 13) ^ std::rotr(a[0], 22));
+	    uint32_t s0 = (rotr(a[0], 2) ^ rotr(a[0], 13) ^ rotr(a[0], 22));
 	    uint32_t maj = (a[0] & a[1]) ^ (a[0] & a[2]) ^ (a[1] & a[2]);
 	    uint32_t temp2 = s0 + maj;
 	    for (int i = 7; i > 0; i--) a[i] = a[i-1];
@@ -89,13 +101,13 @@ void sha_ctx::process(uint8_t* bytes) {
 }
 
 
-void std_read(const char* path)
-{
+void std_read(const char* path) {
     sha_ctx sha(100);    
     constexpr size_t BUFFER_SIZE = 512 * 1024;
     int fd = open(path, O_RDONLY | O_NONBLOCK);
-    fcntl(fd, F_RDADVISE);
-    uint8_t buf[BUFFER_SIZE];
+    // fcntl(fd, F_RDADVISE);
+    uint8_t* buf;
+    cudaMallocManaged((void**)&buf, BUFFER_SIZE);
     while(size_t bytes_read = read(fd, buf, BUFFER_SIZE))
     {
         if(bytes_read == (size_t)-1) {
@@ -103,7 +115,7 @@ void std_read(const char* path)
             exit(1);
         }
         if (!bytes_read) break;
-	sha.process(buf);
+	process<<<1, 1>>>(buf, sha.hash);
     }
 }
 
@@ -112,7 +124,7 @@ int main(int argc, char** argv) {
     // 	std::cout << "usage: ./sha <path>" << "\n";
     // 	exit(0);
     // }
-    std_read("/Users/davfrei/input");
+    std_read("/home/quantumish/events.gif");
     // std::ifstream file(argv[1], std::ios::binary | std::ios::ate);
     // std::streamsize size = file.tellg();
     // file.seekg(0, std::ios::beg);
