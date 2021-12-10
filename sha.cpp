9cebc5ed56d734f1a53b6a816b8ac62f3a97f7dc
David Freifeld <freifeld.david@gmail.com>
2021-12-09 20:38:57 -0800

    Beginning major refactoring

--- sha.cpp
+++ sha.cpp
@@ -10,9 +10,12 @@
 #include <ios>
 #include <string>
 #include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include <fmt/format.h>
-
-using Hash = std::unique_ptr<uint8_t[]>;
 
 template<typename T>
 void dump_array(T* bytes, size_t len) {
@@ -24,36 +27,41 @@ void dump_array(T* bytes, size_t len) {
     std::cout << '\n';
 }
 
-// Blatantly adapted from https://qvault.io/cryptography/how-sha-2-works-step-by-step-sha-256/
-Hash sha_256(char* bytes, uint64_t len) {
-    size_t content_len = len + 1 + 8;
-    size_t buffer_len = 64 * ((content_len / 64) + 1);
-    uint8_t* buffer = (uint8_t*)calloc(buffer_len, sizeof(uint8_t));
-    memcpy(buffer, bytes, len);
-    buffer[len] = 0b10000000;
+constexpr uint64_t k[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+};
 
-    for (int i = 1; i <= 8; i++) buffer[buffer_len-i] = len*8 >> (i-1)*8;
+using Hash = std::unique_ptr<uint8_t[]>;
 
-    uint32_t h[8] = {
+struct sha_ctx {
+    // std::unique_ptr<uint8_t[]> buffer;
+    uint32_t hash[8] = {
 	0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
 	0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
     };
+    uint64_t len;
+    
+    sha_ctx(uint64_t len);
+    void process(uint8_t* bytes);
+    void finalize(uint8_t* bytes);
+    void dump_hash();
+};
 
-    const uint64_t k[64] = {
-	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-    };
+sha_ctx::sha_ctx(uint64_t length) :hash{0}, len(length) {}
 
-    #pragma omp parallel for
-    for (int i = 0; i < buffer_len; i += 64) {
+#define NUM_CHUNKS 8192
+
+void sha_ctx::process(uint8_t* bytes) {
+    for (int i = 0; i < NUM_CHUNKS; i += 1) {
 	uint32_t w[64] = {0};
-	memcpy(w, buffer+i, 64);	
+	memcpy(w, bytes+(i*64), 64);
 	for (int i = 0; i < 16; i++) w[i] = __builtin_bswap32(w[i]);
 
 	for (int i = 16; i < 64; i++) {
@@ -63,7 +71,7 @@ Hash sha_256(char* bytes, uint64_t len) {
 	}
 
 	uint32_t a[8] = {0};
-	memcpy(a, h, sizeof(uint32_t)*8);
+	memcpy(a, hash, sizeof(uint32_t)*8);
 
 	for (int i = 0; i < 64; i++) {
 	    uint32_t s1 = (std::rotr(a[4], 6) ^ std::rotr(a[4], 11) ^ std::rotr(a[4], 25));
@@ -76,33 +84,43 @@ Hash sha_256(char* bytes, uint64_t len) {
 	    a[4] += temp1;
 	    a[0] = temp1 + temp2;
 	}	
-	for (int i = 0; i < 8; i++) h[i] = h[i] + a[i];
+	for (int i = 0; i < 8; i++) hash[i] = hash[i] + a[i];
     }
+}
 
-    std::unique_ptr<uint8_t[]> out(new uint8_t[32]());
-    memcpy(out.get(), h, sizeof(uint32_t)*8);
 
-    auto u32_ptr = reinterpret_cast<uint32_t*>(out.get());
-    for (int i = 0; i < 8; i++) {
-	*(u32_ptr+i) = __builtin_bswap32(*(u32_ptr+i));
+void std_read(const char* path)
+{
+    sha_ctx sha(100);    
+    constexpr size_t BUFFER_SIZE = 512 * 1024;
+    int fd = open(path, O_RDONLY | O_NONBLOCK);
+    fcntl(fd, F_RDADVISE);
+    uint8_t buf[BUFFER_SIZE];
+    while(size_t bytes_read = read(fd, buf, BUFFER_SIZE))
+    {
+        if(bytes_read == (size_t)-1) {
+            printf("bytes_read == (size_t)-1\n");
+            exit(1);
+        }
+        if (!bytes_read) break;
+	sha.process(buf);
     }
-
-    return out;
 }
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-	std::cout << "usage: ./sha <path>" << "\n";
-	exit(0);
-    }
-    std::ifstream file(argv[1], std::ios::binary | std::ios::ate);
-    std::streamsize size = file.tellg();
-    file.seekg(0, std::ios::beg);
-    char* buf = (char*)malloc(size);
-    file.read(buf, size);
-    auto hash = sha_256(buf, size);
-    for (int i = 0; i < 32; i++) {
-	std::cout << fmt::format("{:02x}", static_cast<uint8_t>(hash[i]));
-    }
-    std::cout << "  " << argv[1] << "\n";
+    // if (argc != 2) {
+    // 	std::cout << "usage: ./sha <path>" << "\n";
+    // 	exit(0);
+    // }
+    std_read("/Users/davfrei/input");
+    // std::ifstream file(argv[1], std::ios::binary | std::ios::ate);
+    // std::streamsize size = file.tellg();
+    // file.seekg(0, std::ios::beg);
+    // char* buf = (char*)malloc(size);
+    // file.read(buf, size);
+    // auto hash = sha_256(buf, size);
+    // for (int i = 0; i < 32; i++) {
+    // 	std::cout << fmt::format("{:02x}", static_cast<uint8_t>(hash[i]));
+    // }
+    // std::cout << "  " << argv[1] << "\n";
 }
