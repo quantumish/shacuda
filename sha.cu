131a7fd0f03cb22e4993c0f7d49f84dc9f13c3a6
David Freifeld <freifeld.david@gmail.com>
2021-12-12 00:16:09 -0800

    GPU code is at least sane
    It is now deterministic

--- sha.cu
+++ sha.cu
@@ -82,75 +82,39 @@ __global__ void process(uint8_t* bytes, uint32_t* hash) {
     uint32_t w[64] = {0};
     memcpy(w, bytes+(threadIdx.x*64), 64);
     for (int i = 0; i < 16; i++) {
+	// bswap32
 	w[i] = ((w[i]>>24)&0xff) | // move byte 3 to byte 0
 	    ((w[i]<<8)&0xff0000) | // move byte 1 to byte 2
 	    ((w[i]>>8)&0xff00) | // move byte 2 to byte 1
 	    ((w[i]<<24)&0xff000000); // byte 0 to byte 3
-
-	for (int i = 16; i < 64; i++) {
-	    uint32_t s0 = (rotr(w[i-15], 7) ^ rotr(w[i-15], 18) ^ (w[i-15] >> 3));
-	    uint32_t s1 = (rotr(w[i-2], 17) ^ rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
-	    w[i] = w[i-16] + s0 + w[i-7] + s1;
-	}
-	// dump_array(w, 64);
-
-	uint32_t a[8] = {0};
-	memcpy(a, hash, sizeof(uint32_t)*8);
-
-	for (int i = 0; i < 64; i++) {
-	    uint32_t s1 = (rotr(a[4], 6) ^ rotr(a[4], 11) ^ rotr(a[4], 25));
-	    uint32_t ch = (a[4] & a[5]) ^ ((~a[4]) & a[6]);
-	    uint32_t temp1 = a[7] + s1 + ch + k[i] + w[i];
-	    uint32_t s0 = (rotr(a[0], 2) ^ rotr(a[0], 13) ^ rotr(a[0], 22));
-	    uint32_t maj = (a[0] & a[1]) ^ (a[0] & a[2]) ^ (a[1] & a[2]);
-	    uint32_t temp2 = s0 + maj;
-	    for (int i = 7; i > 0; i--) a[i] = a[i-1];
-	    a[4] += temp1;
-	    a[0] = temp1 + temp2;
-	}
-	for (int i = 0; i < 8; i++) hash[i] = hash[i] + a[i];
-    }
-}
-
-void cpu_process(uint8_t* bytes, uint32_t* hash) {
-    uint32_t w[64] = {0};
-    memcpy(w, bytes, 64);
-    for (int i = 0; i < 16; i++) {
-      w[i] = __builtin_bswap32(w[i]);
     }
     
-    // w[i] = ((w[i]>>24)&0xff) | // move byte 3 to byte 0
-    //   ((w[i]<<8)&0xff0000) | // move byte 1 to byte 2
-    //   ((w[i]>>8)&0xff00) | // move byte 2 to byte 1
-    //   ((w[i]<<24)&0xff000000); // byte 0 to byte 3
-
     for (int i = 16; i < 64; i++) {
-        uint32_t s0 = (rotr(w[i-15], 7) ^ rotr(w[i-15], 18) ^ (w[i-15] >> 3));
+	uint32_t s0 = (rotr(w[i-15], 7) ^ rotr(w[i-15], 18) ^ (w[i-15] >> 3));
 	uint32_t s1 = (rotr(w[i-2], 17) ^ rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
-	printf("%02x\n", w[i-16] + s0 + w[i-7] + s1);
 	w[i] = w[i-16] + s0 + w[i-7] + s1;
     }
-    pdump_array<uint32_t>(w, 64);
+    // dump_array(w, 64);
     
     uint32_t a[8] = {0};
     memcpy(a, hash, sizeof(uint32_t)*8);
     
     for (int i = 0; i < 64; i++) {
-      uint32_t s1 = (rotr(a[4], 6) ^ rotr(a[4], 11) ^ rotr(a[4], 25));
-      uint32_t ch = (a[4] & a[5]) ^ ((~a[4]) & a[6]);
-      uint32_t temp1 = a[7] + s1 + ch + cpu_k[i] + w[i];
-      uint32_t s0 = (rotr(a[0], 2) ^ rotr(a[0], 13) ^ rotr(a[0], 22));
-      uint32_t maj = (a[0] & a[1]) ^ (a[0] & a[2]) ^ (a[1] & a[2]);
-      uint32_t temp2 = s0 + maj;
-      for (int i = 7; i > 0; i--) a[i] = a[i-1];
-      a[4] += temp1;
-      a[0] = temp1 + temp2;
+	uint32_t s1 = (rotr(a[4], 6) ^ rotr(a[4], 11) ^ rotr(a[4], 25));
+	uint32_t ch = (a[4] & a[5]) ^ ((~a[4]) & a[6]);
+	uint32_t temp1 = a[7] + s1 + ch + k[i] + w[i];
+	uint32_t s0 = (rotr(a[0], 2) ^ rotr(a[0], 13) ^ rotr(a[0], 22));
+	uint32_t maj = (a[0] & a[1]) ^ (a[0] & a[2]) ^ (a[1] & a[2]);
+	uint32_t temp2 = s0 + maj;
+	for (int i = 7; i > 0; i--) a[i] = a[i-1];
+	a[4] += temp1;
+	a[0] = temp1 + temp2;
     }
-    for (int i = 0; i < 8; i++) hash[i] = hash[i] + a[i];
+    for (int i = 0; i < 8; i++) atomicAdd(hash+i, a[i]);
 }
 
 int main(int argc, char** argv) {
-    sha_ctx sha(10);
+    sha_ctx sha(11559122);
     cudaMallocManaged((void**)&sha.hash, 8*sizeof(uint32_t));
     sha.hash[0] = 0x6a09e667;
     sha.hash[1] = 0xbb67ae85;
@@ -162,7 +126,7 @@ int main(int argc, char** argv) {
     sha.hash[7] = 0x5be0cd19;
 
     constexpr size_t BUFFER_SIZE = 32*1024;
-    int fd = open("/home/quantumish/projects/shacuda/.gitignore", O_RDONLY | O_NONBLOCK);
+    int fd = open("/home/quantumish/events.gif", O_RDONLY | O_NONBLOCK);
     // fcntl(fd, F_RDADVISE);
     uint8_t* buf;
     cudaMallocManaged((void**)&buf, BUFFER_SIZE);
@@ -176,10 +140,11 @@ int main(int argc, char** argv) {
 	else if (bytes_read < BUFFER_SIZE) {
 	    buf[bytes_read] = 0b10000000;
 	    size_t buffer_len = 64 * (((bytes_read + 9) / 64) + 1);
-	    printf("%zu\n", buffer_len);
 	    for (int i = 1; i <= 8; i++) buf[buffer_len-i] = sha.len*8 >> (i-1)*8;
 	    std::cout << buffer_len/64 << "\n";
-	    cpu_process(buf, sha.hash);
+	    // cpu_process(buf, sha.hash);
+	    process<<<1, 1>>>(buf, sha.hash);
+	    cudaDeviceSynchronize();
 	    break;
 	}
 	else {
