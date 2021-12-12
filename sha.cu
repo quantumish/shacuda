46539056af5427d2a5396f05e8ac63f8743ea1b8
David Freifeld <freifeld.david@gmail.com>
2021-12-11 23:24:13 -0800

    Debugging GPU SHA

--- sha.cu
+++ sha.cu
@@ -18,20 +18,28 @@
 #include <fmt/format.h>
 #include <cuda_runtime_api.h>
 #include <cuda.h>
+#include <stdio.h>
 
 #define rotr(a,b) (((a) >> (b)) | ((a) << (32-(b))))
 
-template<typename T>
-void dump_array(T* bytes, size_t len) {
-    for (size_t i = 1; i < len+1; i++) {
-	auto str = "{:0"+std::to_string(sizeof(T)*8)+"b} ";
-	std::cout << fmt::vformat(str, fmt::make_format_args(*(bytes+i-1)));
-	if (i % (64/(sizeof(T)*8)) == 0) std::cout << '\n';
-    }
-    std::cout << '\n';
-}
+#define dump_array(arr, len) for (size_t i = 1; i < len+1; i++) {	\
+    printf("%02x ", *(bytes+i-1));					\
+    if (i % 32 == 0) printf("\n");					\
+  }									\
+  printf("\n")
 
 __constant__ uint64_t k[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+};
+
+uint64_t cpu_k[64] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -74,6 +82,7 @@ __global__ void process(uint8_t* bytes, uint32_t* hash) {
 	    uint32_t s1 = (rotr(w[i-2], 17) ^ rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
 	    w[i] = w[i-16] + s0 + w[i-7] + s1;
 	}
+	// dump_array(w, 64);
 
 	uint32_t a[8] = {0};
 	memcpy(a, hash, sizeof(uint32_t)*8);
@@ -93,8 +102,40 @@ __global__ void process(uint8_t* bytes, uint32_t* hash) {
     }
 }
 
+void cpu_process(uint8_t* bytes, uint32_t* hash) {
+    uint32_t w[64] = {0};
+    memcpy(w, bytes, 64);
+    // w[i] = ((w[i]>>24)&0xff) | // move byte 3 to byte 0
+    //   ((w[i]<<8)&0xff0000) | // move byte 1 to byte 2
+    //   ((w[i]>>8)&0xff00) | // move byte 2 to byte 1
+    //   ((w[i]<<24)&0xff000000); // byte 0 to byte 3
+
+    for (int i = 16; i < 64; i++) {
+      uint32_t s0 = (rotr(w[i-15], 7) ^ rotr(w[i-15], 18) ^ (w[i-15] >> 3));
+      uint32_t s1 = (rotr(w[i-2], 17) ^ rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
+      w[i] = w[i-16] + s0 + w[i-7] + s1;
+    }
+    dump_array<uint32_t>(w, 64);
+    
+    uint32_t a[8] = {0};
+    memcpy(a, hash, sizeof(uint32_t)*8);
+    
+    for (int i = 0; i < 64; i++) {
+      uint32_t s1 = (rotr(a[4], 6) ^ rotr(a[4], 11) ^ rotr(a[4], 25));
+      uint32_t ch = (a[4] & a[5]) ^ ((~a[4]) & a[6]);
+      uint32_t temp1 = a[7] + s1 + ch + cpu_k[i] + w[i];
+      uint32_t s0 = (rotr(a[0], 2) ^ rotr(a[0], 13) ^ rotr(a[0], 22));
+      uint32_t maj = (a[0] & a[1]) ^ (a[0] & a[2]) ^ (a[1] & a[2]);
+      uint32_t temp2 = s0 + maj;
+      for (int i = 7; i > 0; i--) a[i] = a[i-1];
+      a[4] += temp1;
+      a[0] = temp1 + temp2;
+    }
+    for (int i = 0; i < 8; i++) hash[i] = hash[i] + a[i];
+}
+
 int main(int argc, char** argv) {
-    sha_ctx sha(11559122);
+    sha_ctx sha(10);
     cudaMallocManaged((void**)&sha.hash, 8*sizeof(uint32_t));
     sha.hash[0] = 0x6a09e667;
     sha.hash[1] = 0xbb67ae85;
@@ -106,7 +147,7 @@ int main(int argc, char** argv) {
     sha.hash[7] = 0x5be0cd19;
 
     constexpr size_t BUFFER_SIZE = 32*1024;
-    int fd = open("/home/quantumish/l_oneapi_vtune_p_2021.4.0.364_offline.sh", O_RDONLY | O_NONBLOCK);
+    int fd = open("/home/quantumish/projects/shacuda/.gitignore", O_RDONLY | O_NONBLOCK);
     // fcntl(fd, F_RDADVISE);
     uint8_t* buf;
     cudaMallocManaged((void**)&buf, BUFFER_SIZE);
@@ -120,13 +161,15 @@ int main(int argc, char** argv) {
 	else if (bytes_read < BUFFER_SIZE) {
 	    buf[bytes_read] = 0b10000000;
 	    size_t buffer_len = 64 * (((bytes_read + 9) / 64) + 1);
+	    printf("%zu\n", buffer_len);
 	    for (int i = 1; i <= 8; i++) buf[buffer_len-i] = sha.len*8 >> (i-1)*8;
-	    process<<<1, buffer_len/64>>>(buf, sha.hash);
-	    cudaDeviceSynchronize();
+	    std::cout << buffer_len/64 << "\n";
+	    cpu_process(buf, sha.hash);
+	    break;
 	}
-	else {	    
-	    process<<<1, 512>>>(buf, sha.hash);
-	    cudaDeviceSynchronize();
+	else {
+	  process<<<1, 512>>>(buf, sha.hash);
+	  cudaDeviceSynchronize();
 	}
     } while (bytes_read = read(fd, buf, BUFFER_SIZE));
     auto whee = reinterpret_cast<uint8_t*>(sha.hash);
