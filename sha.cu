a9f4508c60eb6920073be195f88d754217373c53
David Freifeld <freifeld.david@gmail.com>
2021-12-15 23:04:48 -0800

    Working-ish GPU code

--- sha.cu
+++ sha.cu
@@ -1,64 +1,14 @@
 #include <cstdint>
-#include <cstddef>
 #include <cstring>
-#include <cassert>
-#include <array>
-#include <bit>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <ios>
-#include <string>
-#include <vector>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
-#include <fmt/format.h>
-#include <cuda_runtime_api.h>
-#include <cuda.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <cuda_runtime_api.h>
+#include <cuda.h>
 
 #define rotr(a,b) (((a) >> (b)) | ((a) << (32-(b))))
-
-// Hacky %b for printf from stackoverflow
-//https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
-//https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format/25108449#25108449
-#define PRINTF_BINARY_SEPARATOR ""
-#define PRINTF_BINARY_PATTERN_INT8 "%c%c%c%c%c%c%c%c"
-#define PRINTF_BYTE_TO_BINARY_INT8(BYTE)	\
-    (((BYTE) & 0x80ll) ? '1' : '0'),		\
-    (((BYTE) & 0x40ll) ? '1' : '0'),		\
-    (((BYTE) & 0x20ll) ? '1' : '0'),		\
-    (((BYTE) & 0x10ll) ? '1' : '0'),		\
-    (((BYTE) & 0x08ll) ? '1' : '0'),		\
-    (((BYTE) & 0x04ll) ? '1' : '0'),		\
-    (((BYTE) & 0x02ll) ? '1' : '0'),		\
-    (((BYTE) & 0x01ll) ? '1' : '0')
-
-#define PRINTF_BINARY_PATTERN_INT16 \
-    PRINTF_BINARY_PATTERN_INT8               PRINTF_BINARY_SEPARATOR              PRINTF_BINARY_PATTERN_INT8
-#define PRINTF_BYTE_TO_BINARY_INT16(BYTE) \
-    PRINTF_BYTE_TO_BINARY_INT8((BYTE) >> 8),   PRINTF_BYTE_TO_BINARY_INT8(BYTE)
-#define PRINTF_BINARY_PATTERN_INT32 \
-    PRINTF_BINARY_PATTERN_INT16              PRINTF_BINARY_SEPARATOR              PRINTF_BINARY_PATTERN_INT16
-#define PRINTF_BYTE_TO_BINARY_INT32(BYTE) \
-    PRINTF_BYTE_TO_BINARY_INT16((BYTE) >> 16), PRINTF_BYTE_TO_BINARY_INT16(BYTE)
-
-#define dump_array32(arr, len) for (size_t DUMP32_ITER = 1; DUMP32_ITER < len+1; DUMP32_ITER++) { \
-	printf("" PRINTF_BINARY_PATTERN_INT32 " ", PRINTF_BYTE_TO_BINARY_INT32(*(arr+DUMP32_ITER-1))); \
-	if (DUMP32_ITER % 2 == 0) printf("\n");				\
-    }									\
-    printf("\n")
-
-#define dump_array8(arr, len) for (size_t DUMP8_ITER = 1; DUMP8_ITER < len+1; DUMP8_ITER++) { \
-	printf("" PRINTF_BINARY_PATTERN_INT8 " ", PRINTF_BYTE_TO_BINARY_INT8(*(arr+DUMP8_ITER-1)));                       \
-	if (DUMP8_ITER % 8 == 0) printf("\n");				\
-    }									\
-    printf("\n")
 
 uint64_t k[64] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -73,8 +23,7 @@ uint64_t k[64] = {
 
 struct sha_ctx {
     uint32_t* hash;
-    uint64_t len;
-
+    uint64_t len;    
     sha_ctx(uint64_t len);
     void compress(uint32_t* w);
     void dump_hash();
@@ -97,20 +46,11 @@ sha_ctx::sha_ctx(uint64_t length) :len(length) {
     ((x>>8)&0xff00) |				\
     ((x<<24)&0xff000000)			\
 
-#define BUFFER_SIZE 268435456
-
-__global__ void process(const uint8_t* bytes, uint32_t* w) {
-    const size_t start_chunk = (blockIdx.y*gridDim.x*blockDim.x)+(blockIdx.x*blockDim.x)+threadIdx.x; 
-    for (int off = 0; off < 1024; off++) { // Doesn't actually iterate 2 times??
-	// printf("wtf\n");
-	// Perhaps sketchy, but shouldn't affect `off` in any way.
-	uint32_t* w_adj = w+(((start_chunk*1024)+off)*64);
-	memcpy(w_adj, bytes+(((start_chunk*1024)+off)*64), 64);
-	assert(w_adj < w+BUFFER_SIZE*4);
-	assert(bytes+(((start_chunk*1024)+off)*64) < bytes+BUFFER_SIZE);
-	assert(w_adj < w+((start_chunk*1024)+1024)*64);
-	assert(bytes+(((start_chunk*1024)+off)*64) < bytes+((start_chunk*1024)+1024)*64);
-	// Logic from here on down is sound.
+__global__ void process(const uint8_t* bytes, uint32_t* w, uint32_t iters) {
+    const size_t start_chunk = (blockIdx.y*gridDim.x*blockDim.x)+(blockIdx.x*blockDim.x)+threadIdx.x;
+    for (int off = 0; off < iters; off++) {
+	uint32_t* w_adj = w+(((start_chunk*iters)+off)*64);
+	memcpy(w_adj, bytes+(((start_chunk*iters)+off)*64), 64);
 	for (int i = 0; i < 16; i++) w_adj[i] = bswap32(w_adj[i]);
 	for (int i = 16; i < 64; i++) {
 	    uint32_t s0 = (rotr(w_adj[i-15], 7) ^ rotr(w_adj[i-15], 18) ^ (w_adj[i-15] >> 3));
@@ -119,7 +59,6 @@ __global__ void process(const uint8_t* bytes, uint32_t* w) {
 	}
     }
 }
-
 
 void sha_ctx::compress(uint32_t* w) {
     uint32_t a[8] = {0};
@@ -143,10 +82,7 @@ void sha_ctx::dump_hash() {
     memcpy(temp_hash, hash, 8*sizeof(uint32_t));
     for (int i = 0; i < 8; i++) temp_hash[i] = bswap32(temp_hash[i]);
     auto u8_ptr = reinterpret_cast<uint8_t*>(temp_hash);
-    for (int i = 0; i < 32; i++) {
-	std::cout << fmt::format("{:02x}", u8_ptr[i]);
-    }
-    std::cout << "\n";
+    for (int i = 0; i < 32; i++) printf("%02x", u8_ptr[i]);
 }
 
 int main(int argc, char** argv) {
@@ -156,13 +92,13 @@ int main(int argc, char** argv) {
     fstat(fd, &stat);
 
     sha_ctx sha(stat.st_size);
-
-    uint8_t* buf;    
+    const size_t BUFFER_SIZE = 268435456;
+    uint8_t* buf;
     cudaMallocManaged(&buf, BUFFER_SIZE);
     uint32_t* w;
     cudaMallocManaged(&w, BUFFER_SIZE*4);
-    std::cout << "alloc'd " << (float)(BUFFER_SIZE*5)/1048576 << " MiB\n";
     size_t bytes_read = read(fd, buf, BUFFER_SIZE);
+    bool padded = false;
     do {
 	if (bytes_read == (size_t)-1) {
 	    printf("Error reading file.");
@@ -174,32 +110,41 @@ int main(int argc, char** argv) {
 	    for (size_t i = bytes_read; i < buffer_len; i++) buf[i] = 0;
 	    buf[bytes_read] = 0b10000000;
 	    for (int i = 1; i <= 8; i++) buf[buffer_len-i] = sha.len*8 >> (i-1)*8;
-	    size_t num_groups = (buffer_len/64)/(1024*1024);
-	    size_t group_shift = 1024*1024*64;
-	    for (int i = 0; i < (buffer_len/64)/(1024*1024); i++) {
-		std::cout << "spawned cleanup kernel\n";
-		process<<<1, 1024>>>(buf+(i*group_shift), w+(i*group_shift));
+	    int threads[3] = {1024, 128, 1024};
+	    int iters[3] = {1024, 1024, 1};
+	    size_t remaining = buffer_len/64;
+	    size_t shift = 0;
+	    for (int i = 0; i < 3; i++) {
+		size_t num_groups = remaining/(threads[i]*iters[i]);
+		remaining = remaining % (threads[i]*iters[i]);
+		for (int j = 0; j < num_groups; j++) {
+		    process<<<1, threads[i]>>>(buf+shift, w+shift, iters[i]);
+		    shift += threads[i]*iters[i]*64;
+		}
 	    }
-	    std::cout << "spawned kernel\n";
-	    std::cout << (buffer_len/64)%(1024*1024) << '\n';
-	    process<<<1, (buffer_len/64)%(1024*1024)>>>(buf+num_groups*group_shift, w+num_groups*group_shift);
+	    process<<<1, remaining>>>(buf+shift, w+shift, 1);
 	    cudaDeviceSynchronize();
 	    for (int i = 0; i < buffer_len/64; i++) sha.compress(w+(i*64));
+	    padded=true;
 	} else {
-	    std::cout << "spawned kernel (main)\n";
-	    process<<<dim3{8,8,1}, 64>>>(buf, w);
+	    process<<<dim3{8,8,1}, 64>>>(buf, w, 1024);
 	    cudaDeviceSynchronize();
-	    
-	    cudaError_t err = cudaGetLastError();
-	    if (err != cudaSuccess) printf("%s\n", cudaGetErrorString(err));
-	    
 	    for (int i = 0; i < BUFFER_SIZE/64; i++) sha.compress(w+(i*64));
 	}
     } while ((bytes_read = read(fd, buf, BUFFER_SIZE)));
-    for (int i = 0; i < 8; i++) sha.hash[i] = bswap32(sha.hash[i]);
-    auto u8_ptr = reinterpret_cast<uint8_t*>(sha.hash);
-    for (int i = 0; i < 32; i++) {
-	std::cout << fmt::format("{:02x}", u8_ptr[i]);
+    if (padded == false) {
+	for (size_t i = 0; i < 64; i++) buf[i] = 0;
+	buf[0] = 0b10000000;
+	for (int i = 1; i <= 8; i++) buf[64-i] = sha.len*8 >> (i-1)*8;
+	memcpy(w, buf, 64);
+	for (int i = 0; i < 16; i++) w[i] = bswap32(w[i]);
+	for (int i = 16; i < 64; i++) {
+	    uint32_t s0 = (rotr(w[i-15], 7) ^ rotr(w[i-15], 18) ^ (w[i-15] >> 3));
+	    uint32_t s1 = (rotr(w[i-2], 17) ^ rotr(w[i-2], 19)  ^ (w[i-2] >> 10));
+	    w[i] = w[i-16] + s0 + w[i-7] + s1;
+	}
+	sha.compress(w);
     }
-    std::cout << "  " << argv[1] << "\n";
+    sha.dump_hash();
+    printf("  %s\n", argv[1]);
 }
